b23db0eca156f376fba66c03677c27608d7e2a16
Josephine Langer <josephine.langer@gmx.de>
2024-03-12 17:05:17 +0100

    changed parameter for better readability

--- src/TrilingualDemocracy.java
+++ src/TrilingualDemocracy.java
@@ -2,7 +2,7 @@
 Trilingual Democracy
 
 """
-Switzerland has four official languages: German, French, Italian, and Romansh.
+Switzerland has four official languages: German [D], French [F], Italian [I], and Romansh [R].
 
 When native speakers of one or more of these languages meet,
 they follow certain regulations to choose a language for the group.
@@ -30,15 +30,15 @@ public class TrilingualDemocracy {
     public String chooseLanguage(String speakers) {
         //each letter in speakers string symbolizes a language spoken
         //save each letter to a separate field
-        //and compare the fields for equality
+        //and compare the fields
         String[] letters = speakers.split("");
 
-        //if(all letters equal each other) return first letter
+        //if(a=b and b=c => all letters are equal) return a
         if (letters[0].equals(letters[1]) &&
                 letters[2].equals(letters[1]))
             return letters[0];
 
-        //if(no letters equal each other) return letter which isn't in the array
+        //if(a!=b and a!=c and b!=c => all letters are different) return letter d which isn't in the array
         if (!(letters[0].equals(letters[1])) &&
                 !(letters[0].equals(letters[2])) &&
                 !(letters[1].equals(letters[2]))
@@ -47,11 +47,10 @@ public class TrilingualDemocracy {
             List<String> languageList = new ArrayList<>(List.of("D", "F", "I", "R"));
 
             //remove all letters from languageList which appear in letter array
-            //(removeAll() takes collections as arguments -> turn array into collection)
-            languageList.removeAll(Arrays.asList(letters));
+            languageList.removeAll(List.of(letters));
 
             //return languageList.toString(); //has cosmetic output issues
-            //turn list into array again to resolve cosmetic output issues
+            //convert list into array to resolve cosmetic output issues
             String[] arrLanguageLeft = languageList.toArray(String[]::new);
             return arrLanguageLeft[0];
         }
@@ -74,5 +73,6 @@ public class TrilingualDemocracy {
         System.out.println("IIR: " + tD.chooseLanguage("IIR"));
         System.out.println("FDF: " + tD.chooseLanguage("FDF"));
         System.out.println("FRR: " + tD.chooseLanguage("FRR"));
+        System.out.println("IDF: " + tD.chooseLanguage("IDF"));
     }
 }

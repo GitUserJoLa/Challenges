5d9f71e9359574084fa671aedd1a020df298a619
Josephine Langer <josephine.langer@gmx.de>
2024-03-14 15:59:03 +0100

    pulled basic TreeNode class from codewars https://www.codewars.com/kata/57dd79bff6df9b103b00010f/train/java credits to janitormeir
     Please enter the commit message for your changes. Lines starting
    i#

---
+++ src/TreeNode.java
@@ -0,0 +1,75 @@
+/*
+You are given a class called TreeNode.
+Implement the method isPerfect() which determines if a given tree denoted by its root node is perfect.
+https://www.codewars.com/kata/57dd79bff6df9b103b00010f
+ Credits to janitormeir
+ */
+public class TreeNode {
+    TreeNode left;
+    TreeNode right;
+    int value;
+
+    /*
+    TreeNode(){}
+     */
+    /*
+    TreeNode(int value, TreeNode left, TreeNode right) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+
+     */
+
+    /*
+    TreeNode(int value) {
+        this(value, null, null);
+    }
+
+     */
+
+    public static boolean isPerfect(TreeNode root) {
+        return false; // TODO: implementation
+    }
+    static TreeNode leaf() {
+        return new TreeNode();
+    }
+
+    static TreeNode join(TreeNode left, TreeNode right) {
+        return new TreeNode().withChildren(left, right);
+    }
+
+    TreeNode withLeft(TreeNode left) {
+        this.left = left;
+        return this;
+    }
+
+    TreeNode withRight(TreeNode right) {
+        this.right = right;
+        return this;
+    }
+
+    TreeNode withChildren(TreeNode left, TreeNode right) {
+        this.left = left;
+        this.right = right;
+        return this;
+    }
+
+    TreeNode withLeftLeaf() {
+        return withLeft(leaf());
+    }
+
+    TreeNode withRightLeaf() {
+        return withRight(leaf());
+    }
+
+    TreeNode withLeaves() {
+        return withChildren(leaf(), leaf());
+    }
+    @Override
+    public boolean equals(Object other) {
+        // TODO: IMPLEMENTATION
+        // Already implemented for you and used in test cases
+        return false;
+    }
+}

ed8aa5622d2a40f154152bd3e58de3d5da8f4270
Josephine Langer <josephine.langer@gmx.de>
2024-03-14 16:26:26 +0100

    added k-prime-problem all k-related functions have to be implemented prime factor calculation has to be implemented

--- src/PrimeNumberCalc.java
+++ src/PrimeNumberCalc.java
@@ -6,13 +6,56 @@ public class PrimeNumberCalc {
     private PrimeNumberCalc() {
     }
 
-    public static boolean isPrime(Integer potPrime) {
+    public static List<Integer> primeFactors(Integer number) {
+        // TODO: IMPLEMENTATION
+        return null;
+    }
+
+    public static boolean isPrime(Integer number) {
         //Time complexity: O(sqrt(n))
-        for (int i = 2; i * i <= potPrime; i++)
-            if (potPrime % i == 0)
+        for (int i = 2; i * i <= number; i++)
+            if (number % i == 0)
                 return false;
         return true;
     }
+
+    public static boolean isKPrime(Integer k) {
+        // TODO: IMPLEMENTATION
+        /*
+        A natural number is called k-prime if it has exactly k prime factors, counted with multiplicity.
+
+        A natural number is thus prime if and only if it is 1-prime.
+
+        Examples of k-primes:
+        k = 2 -> 4, 6, 9, 10, 14, 15, 21, 22, …
+        k = 3 -> 8, 12, 18, 20, 27, 28, 30, …
+        k = 5 -> 32, 48, 72, 80, 108, 112, …
+
+        https://www.codewars.com/kata/5a48948e145c46820b00002f
+         */
+        return false;
+    }
+
+    public static Integer[][] kPrimesStep(Integer k, Integer step, Integer start, Integer end) {
+        // TODO: IMPLEMENTATION
+
+         /*
+        We will write a function kprimes_step(k, step, start, nd) with parameters:
+
+        k (integer > 0) which indicates the type of k-primes we are looking for,
+        step (integer > 0) which indicates the step we want to find between two k-primes,
+        start (integer >= 0) which gives the start of the search (start inclusive),
+        nd (integer >= start) which gives the end of the search (nd inclusive)
+
+        In the example above kprimes_step(2, 2, 0, 50) will return  [[4, 6], [33, 35]]
+        which are the pairs of 2-primes between 2 and 50 with a 2 steps.
+        So this function should return an array of all the pairs (or tuples) of k-prime numbers
+        spaced with a step of step between the limits start, nd. This array can be empty.
+        https://www.codewars.com/kata/5a48948e145c46820b00002f
+        */
+        return null;
+    }
+
 
     public static @NotNull Set<Integer> multiplesOfN(int n, int limit) {
         Set<Integer> multiples = new TreeSet<>();
@@ -68,8 +111,9 @@ public class PrimeNumberCalc {
          */
     }
 
-    //uncompleted
     public static @NotNull ArrayList<Integer> sieveOfAtkins(Integer number) {
+        // TODO: IMPLEMENTATION
+
         /*
          * Use 'Sieve of Atkin' algorithm
          *

4ab1380b667f1ac11fe54e51dd2440997acdfc18
Josephine Langer <josephine.langer@gmx.de>
2024-04-10 18:15:22 +0200

    - BinaryTree: debugging in progress, commit to save current status before checkout of older commit to see if the bug existed in older version

--- src/binarytrees/BinaryTree.java
+++ src/binarytrees/BinaryTree.java
@@ -4,7 +4,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class BinaryTree {
-    private Node root;
+    private Node root = null;
 
     Node getRoot() {
         return root;
@@ -15,7 +15,6 @@ public class BinaryTree {
     }
 
     private @Nullable Node binarySearch(int value, Node pointerNode) {
-
         if (pointerNode == null) {
             System.out.println("Could not find Node with value of " + value);
             return null;
@@ -29,26 +28,30 @@ public class BinaryTree {
     }
 
     public Node insert(int value) {
-        root = insert(value, root);
-        return root;
+        Node insertedNode = insert(value, root);
+//        System.out.println("insertedNodeValue: " + insertedNode.value + "; insertedNodeAddress: " + insertedNode);
+        if (root == null)
+            root = insertedNode;
+        return insertedNode;
     }
 
     private @NotNull Node insert(int value, Node pointerNode) {
         if (pointerNode == null) {
-            pointerNode = new Node(value);
+            return new Node(value);
         } else if (value < pointerNode.value) {
             Node newNode = insert(value, pointerNode.leftChild);
             pointerNode.setLeftChild(newNode);
+            return newNode;
         } else {
             Node newNode = insert(value, pointerNode.rightChild);
             pointerNode.setRightChild(newNode);
+            return newNode;
         }
-        return pointerNode;
     }
 
     private @Nullable Node searchParent(int value, Node pointerNode) {
         if (pointerNode == null) {
-            System.out.println("Could not find Node with value of " + value);
+            System.out.println("Did not find Node with value of " + value);
             return null;
         }
 
@@ -63,11 +66,17 @@ public class BinaryTree {
 
     public void deleteValue(int value) {
         Node toDelete = binarySearch(value);
-        Node parent = searchParent(value, root);
+//        deleteValue(toDelete);
+    }
 
-        if (toDelete != null)
-            System.out.println("Node to be deleted - nodeValue: " + toDelete.value + ", nodeAddress: " + toDelete +
-                    "\n - parentValue: " + parent.value + ", parentAddress: " + parent);
+    public void deleteValue(Node toDelete) {
+        if (toDelete == null)
+            return;
+        int nodeValue = toDelete.value;
+        Node parent = searchParent(nodeValue, root);
+
+        System.out.println("Node to be deleted - nodeValue: " + toDelete.value + ", nodeAddress: " + toDelete +
+                "\n - parentValue: " + parent.value + ", parentAddress: " + parent);
 
 
         // case 1:  Node doesn't have children -> Node is a leaf
@@ -80,15 +89,12 @@ public class BinaryTree {
 
         // case 3:  Node has two children
 
-        // case 4: Node is root with two children
+        // (case 4: Node is root with two children)
     }
-
 
 //    public int height(){return 0;}
 //    public int numNodes(){return 0;}
 //    public List<Node> toList() {return null;} // mit Lists.toArray() laesst sich easy ein array erzeugen
 //    public BinaryTree balance() {return null;}
 ////    implements Serializable
-
-
 }
